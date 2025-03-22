ebd583a7b02ea36465486e3b4fb5510400fe9fe2
az0990zzzz <adyktktdita@gmail.com>
2025-03-22 16:19:05 +0300

    Update game.js

--- game.js
+++ game.js
@@ -15,35 +15,46 @@ pipeUp.src = "pipeUp.png";
 pipeDown.src = "pipeDown.png";
 
 // Позиция птицы
-let x = 50, y = 150, gravity = 1.5, velocity = 0;
+let x = 50, y = 150, gravity = 0.5, velocity = 0;
 const jump = () => velocity = -8;
 
 document.addEventListener("keydown", jump);
 canvas.addEventListener("click", jump);
 
 // Генерация труб
-const pipes = [{ x: canvas.width, y: Math.random() * 200 - 200 }];
+const pipes = [];
 const pipeGap = 90;
 let score = 0;
+let gameStarted = false;
+
+function spawnPipe() {
+    pipes.push({ x: canvas.width, y: Math.random() * 200 - 200 });
+}
 
 function draw() {
+    if (!gameStarted) return;
+    
     ctx.drawImage(bg, 0, 0);
     
-    pipes.forEach(pipe => {
+    pipes.forEach((pipe, index) => {
         ctx.drawImage(pipeUp, pipe.x, pipe.y);
         ctx.drawImage(pipeDown, pipe.x, pipe.y + pipeUp.height + pipeGap);
         pipe.x -= 2;
+
+        if (pipe.x + pipeUp.width < 0) {
+            pipes.splice(index, 1);
+            score++;
+        }
 
         if (x + bird.width >= pipe.x && x <= pipe.x + pipeUp.width &&
             (y <= pipe.y + pipeUp.height || y + bird.height >= pipe.y + pipeUp.height + pipeGap)) {
             location.reload();
         }
-
-        if (pipe.x === 125) {
-            pipes.push({ x: canvas.width, y: Math.random() * 200 - 200 });
-            score++;
-        }
     });
+
+    if (pipes.length === 0 || pipes[pipes.length - 1].x < canvas.width - 200) {
+        spawnPipe();
+    }
 
     ctx.drawImage(ground, 0, canvas.height - ground.height);
     ctx.drawImage(bird, x, y);
@@ -74,6 +85,7 @@ startButton.style.padding = "10px 20px";
 startButton.style.border = "none";
 startButton.style.cursor = "pointer";
 startButton.onclick = function() {
+    gameStarted = true;
     document.body.removeChild(startButton);
     draw();
 };
